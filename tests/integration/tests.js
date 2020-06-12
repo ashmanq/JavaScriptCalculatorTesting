31f319638558b185e5ec44d13eb20ea9bcc05e61
Ashir <ashirq@outlook.com>
2020-06-12 14:57:07 +0100

    Integration Test: gives unexpected result when dividing by zero; says infinity

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -79,4 +79,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('531441');
   });
 
+  it('should show zero when dividing by zero', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number1')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0');
+  });
+
 });
