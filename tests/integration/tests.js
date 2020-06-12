ff7dc73ab37c75abafbdf635c02d9121bce1fc17
Ashir <ashirq@outlook.com>
2020-06-12 14:46:29 +0100

    Integration tests: running total updates when clicking numbers and artihmetic operations update the display with the result.

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -7,13 +7,30 @@ describe('calculator functionality', function() {
   beforeEach(function() {
     browser.ignoreSynchronization = true;
     browser.get('http://localhost:3000');
+
   });
 
   // write integration tests here in the form of "it should do something..."
   it('should have working number buttons', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+    expect(running_total.getAttribute('value')).to.eventually.equal('2');
   })
+
+  it('should update display of the running total when clicking number', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number3')).click();
+    element(by.css('#number4')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('34');
+  });
+
+  it('should update the display with the result of arithmetic operations', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number3')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('7');
+  });
 
 });
