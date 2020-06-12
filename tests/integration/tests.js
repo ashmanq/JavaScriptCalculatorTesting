e71f66325dbe3d0b1ba1b16d623108379a1834cd
Ashir <ashirq@outlook.com>
2020-06-12 14:54:35 +0100

    Integration Testing: can display negative numbers, numbers with decimal and very large numbers

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -33,7 +33,7 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('7');
   });
 
-  it('should be avle to chain multiple operations together', function() {
+  it('should be able to chain multiple operations together', function() {
     running_total = element(by.css('#running_total'));
     element(by.css('#number2')).click();
     element(by.css('#operator_add')).click();
@@ -42,6 +42,41 @@ describe('calculator functionality', function() {
     element(by.css('#number3')).click();
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('12');
+  });
+
+  it('should be able show negative numbers', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number1')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-3');
+  });
+
+  it('should be able show decimal numbers as result', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number1')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0.5');
+  });
+
+  it('should be able show large numbers as result', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('531441');
   });
 
 });
