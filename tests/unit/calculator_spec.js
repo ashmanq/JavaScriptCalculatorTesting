34174908e6b42d1a7154c2a9d7225c22d8299161
Ashir <ashirq@outlook.com>
2020-06-12 14:05:22 +0100

    Unit test: it can subtract numbers

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -12,9 +12,19 @@ describe('calculator', function () {
   });
 
   it('it can add numbers', function() {
+    // We manually allocate the previous total then add 4 to see if the
+    // running total shows the added result
     calculator.previousTotal = 1;
     calculator.add(4);
     assert.equal(5, calculator.runningTotal);
+  });
+
+  it('it can subtract numbers', function() {
+    // We manually allocate the previous total then subtract 3 to demonstrate
+    // total shows the subtracted result
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    assert.equal(3, calculator.runningTotal)
   });
 
 });
